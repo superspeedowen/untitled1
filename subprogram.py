7ee1adb16eb9fd6097577c20073abcad4a2fcbd8
uallen <superspeedowen@gmail.com>
2016-06-08 13:55:03 +0800

    finish

--- subprogram.py
+++ subprogram.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 def plotData(data,start,end,frequency):
     fig, axs = plt.subplots(4, sharex=True, sharey=True)
     t = np.arange(start,end,1/frequency)
+    plt.ticklabel_format(useOffset=False)
     for n in range(0,4):
         axs[n].plot(t,data[n+1,(start*frequency):(end*frequency)])
     fig.subplots_adjust(hspace=0)
