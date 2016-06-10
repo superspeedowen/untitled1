6001c22d268563f57328fd1c1878c42e2a200ef6
uallen <superspeedowen@gmail.com>
2016-06-10 12:15:11 +0800

    finish

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ file_name = "d533101.dat"
 frequency=10000
 data = np.reshape(np.fromfile(file_name,dtype=np.int16,count=-1),(8,-1), order='F')
 
-subprogram.plotData(data,120,128,frequency)
+subprogram.plotData(data,120,121,frequency)
 #subprogram.plotData(data,120,129,frequency)
 plt.show()
 
