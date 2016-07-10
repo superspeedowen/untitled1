d8c79b12b43a5ebc0dd6fa805ae03133b1acd548
allenu <superspeedowen@gmail.com>
2016-07-10 22:08:10 +0800

    mac

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ import matplotlib.pyplot as plt
 import subprogram as subprogram
 path = '/Users/allenu/Documents/Spike Sorting/DATA Set/CA1/d5331/'
 file_name = "d533101.dat"
-frequency=10000
+sample_frequency=10000
 data = np.reshape(np.fromfile(file_name,dtype=np.int16,count=-1),(8,-1), order='F')
 
-# subprogram.plotData(data,120,128,frequency)
-# #subprogram.plotData(data,120,129,frequency)
+# subprogram.plotData(data,120,128,sample_frequency)
+# #subprogram.plotData(data,120,129,sample_frequency)
 # plt.show()
 
-datasample = data[0:4,0:60*frequency]
+datasample = data[0:4,0:60*sample_frequency]
+
+
+
--- subprogram.py
+++ subprogram.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.signal import butter, lfilter
 
 def plotData(data,start,end,frequency):
     fig, axs = plt.subplots(4, sharex=True, sharey=True)
@@ -9,3 +10,15 @@ def plotData(data,start,end,frequency):
         axs[n].plot(t,data[n+1,(start*frequency):(end*frequency)])
     fig.subplots_adjust(hspace=0)
     plt.setp([a.get_xticklabels() for a in fig.axes[:-1]], visible=False)
+
+def butter_bandpass(lowcut, highcut, fs, order=5):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype='band')
+    return b, a
+
+
+def butter_bandpass_filter(data, b,a):
+    y = lfilter(b, a, data)
+    return y
